f65a0f3b9ce6c9b14ec09c902b0bd9974c6022a2
Anjelika86 <fominaangelika1986@gmail.com>
2021-08-04 17:20:53 +0300

    Add class RangeValidator

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,38 @@
 "use strict";
+
+class RangeValidator {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  set from(newFrom) {
+    if (isNaN(newFrom) || typeof newFrom !== "number") {
+      throw new TypeError("Age must be number");
+    }
+
+    this._from = newFrom;
+  }
+  get from() {
+    return this._from;
+  }
+  set to(newTo) {
+    if (isNaN(newTo) || typeof newTo !== "number") {
+      throw new TypeError("Age must be number");
+    }
+    if (to === i) {
+      throw new RangeError(`From can't be more to`);
+    }
+    this._to = newTo;
+  }
+  get to() {
+    return this._to;
+  }
+  getterRrange() {
+    const arrRange = [];
+    for (let i = this.from; i <= this.to; i++) {
+      arrRange.push(i);
+    }
+    return arrRange;
+  }
+}
+const range = new RangeValidator(0, 10);
